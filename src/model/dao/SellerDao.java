2177abebaf87b9fe70cb787581ce8954dd7ad798
João Victor Costa dos Santos <joaovictorcs29@gmail.com>
2024-04-10 22:50:45 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDao {
+
+    void insert(Seller obj); //insere o objeto enviado como paramentro
+    void update(Seller obj);
+    void deleteById(Seller id);
+    Seller findById(Seller id);
+    List<Seller> findAll();
+}
