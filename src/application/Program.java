e254d0e424063e83c372f82f5be690356d43e234
João Victor Costa dos Santos <joaovictorcs29@gmail.com>
2024-04-12 15:24:21 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,10 @@ public class Program {
         sellerDao.insert(newseller);
         System.out.println("Inserted! New id = " + newseller.getId());
 
+        System.out.println("\n=== TEST 5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Batman Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
